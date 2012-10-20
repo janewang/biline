ee8616246d590fec240d5c915ff18fc75fe74b14
Frank Pinto <frank.joseph.pinto@gmail.com>
2012-10-19 23:16:23 -0400

    Added dom ready event and refactored a bit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var app = express.createServer();
+console.log(__dirname + '/public');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response, next) {
   response.render('index.ejs', {layout: false});
