6259b329c00b5bd6bcfb536763f59fdca3a9c32e
Frank Pinto <frank.joseph.pinto@gmail.com>
2012-10-19 23:34:31 -0400

    Finally integrated index.html into backend. Paperscript is weird. Played around with strokeEnds

--- public/js/draw.js
+++ public/js/draw.js
@@ -3,68 +3,66 @@
 //
 // http://scriptographer.org/tutorials/geometry/working-with-mouse-vectors/#adding-brush-stroke-ends
 
-DomReady.ready(function() {
-  tool.fixedDistance = 80;
+tool.fixedDistance = 80;
 
-  var path;
-  var strokeEnds = 1;
+var path;
+var strokeEnds = 0.5;
 
-  function onMouseDown(event) {
-    path = new Path();
-    path.fillColor = event.count % 2 ? 'red' : 'black';
+function onMouseDown(event) {
+  path = new Path();
+  path.fillColor = event.count % 2 ? 'red' : 'black';
+}
+
+var lastPoint;
+function onMouseDrag(event) {
+  // If this is the first drag event,
+  // add the strokes at the start:
+  if (event.count == 1) {
+    addStrokes(event.middlePoint, event.delta * -1);
+  } else {
+    var step = event.delta / 2;
+    step.angle += 90;
+
+    // The top point: the middle point + the step rotated by 90 degrees:
+    //   -----*
+    //   |
+    //   ------
+    var top = event.middlePoint + step;
+
+    // The bottom point: the middle point - the step rotated by 90 degrees:
+    //   ------
+    //   |
+    //   -----*
+    var bottom = event.middlePoint - step;
+
+    path.add(top);
+    path.insert(0, bottom);
   }
+  path.smooth();
 
-  var lastPoint;
-  function onMouseDrag(event) {
-    // If this is the first drag event,
-    // add the strokes at the start:
-    if (event.count == 1) {
-      addStrokes(event.middlePoint, event.delta * -1);
-    } else {
-      var step = event.delta / 2;
-      step.angle += 90;
+  lastPoint = event.middlePoint;
+}
 
-      // The top point: the middle point + the step rotated by 90 degrees:
-      //   -----*
-      //   |
-      //   ------
-      var top = event.middlePoint + step;
+function onMouseUp(event) {
+  var delta = event.point - lastPoint;
+  delta.length = tool.maxDistance;
+  addStrokes(event.point, delta);
+  path.closed = true;
+  path.smooth();
+}
 
-      // The bottom point: the middle point - the step rotated by 90 degrees:
-      //   ------
-      //   |
-      //   -----*
-      var bottom = event.middlePoint - step;
-
-      path.add(top);
-      path.insert(0, bottom);
+function addStrokes(point, delta) {
+  var step = delta.rotate(90);
+  var strokePoints = strokeEnds * 2 + 1;
+  point -= step / 2;
+  step /= strokePoints - 1;
+  for (var i = 0; i < strokePoints; i++) {
+    var strokePoint = point + step * i;
+    var offset = delta * (Math.random() * 0.3 + 0.1);
+    if (i % 2) {
+      offset *= -1;
     }
-    path.smooth();
-
-    lastPoint = event.middlePoint;
+    strokePoint += offset;
+    path.insert(0, strokePoint);
   }
-
-  function onMouseUp(event) {
-    var delta = event.point - lastPoint;
-    delta.length = tool.maxDistance;
-    addStrokes(event.point, delta);
-    path.closed = true;
-    path.smooth();
-  }
-
-  function addStrokes(point, delta) {
-    var step = delta.rotate(90);
-    var strokePoints = strokeEnds * 2 + 1;
-    point -= step / 2;
-    step /= strokePoints - 1;
-    for (var i = 0; i < strokePoints; i++) {
-      var strokePoint = point + step * i;
-      var offset = delta * (Math.random() * 0.3 + 0.1);
-      if (i % 2) {
-        offset *= -1;
-      }
-      strokePoint += offset;
-      path.insert(0, strokePoint);
-    }
-  }
-});
+}
