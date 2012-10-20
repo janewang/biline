f2ff732b4e8802f864ce7c3359839fca66ddffca
Frank Pinto <frank.joseph.pinto@gmail.com>
2012-10-20 01:18:35 -0400

    Add session, move around socket.io a little, drop in example code

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 #!/usr/local/bin/node
 
-var express   = require('express');
-var socketio  = require('socket.io');
-var app       = express.createServer();
+var express = require('express');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
 
-app.set('view engine', 'ejs');
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+app.use(express.cookieParser());
+app.use(express.session({secret: 'pandas'}));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response, next) {
-  response.render('index', {layout: false});
-  response.end();
+app.get('/', function(req, res, next) {
+  res.render('index.ejs', {layout: false});
+  res.end();
 });
 
 app.listen(3000, function() {
     console.log('Now listening on port 3000');    
 });
-
-var sio = socketio.listen(app, {log: false});
-console.log(sio);
