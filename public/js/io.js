6e5fa3d69a4546afcc6c0cb7d04564d185263c69
Jane Wang <janelwang2008@gmail.com>
2012-10-20 19:18:16 -0400

    Draw on canvas on other players data

--- public/js/io.js
+++ public/js/io.js
@@ -1,7 +1,46 @@
 var socket = io.connect('http://' + window.location.hostname);
+var canvas = document.getElementById('#canvas');
+paper.setup(canvas);
+
+var path;
+var strokeEnds = 0.5;
+var lastPoint;
+
+function onMouseDown() {
+  path = new paper.Path();
+  path.fillColor = 'red'; // other players path are red
+}
+
+function onMouseDrag(data) {
+  /*
+  var setp = data.delta / 2;
+  step.angle += 90;
+  var top = data.middlePoint + 10;
+  var bottom = event.middlePoint - 10;
+  path.add(top);
+  path.insert(0, bottom);
+  path.smooth();
+  lastPoint = data.middlePoint;
+  */
+}
+
+function onMouseUp(data) {
+  /*
+  var delta = data.point - lastPoint;
+  delta.length = paper.tool.maxDistance;
+  addStrokes(data.point, delta);
+  path.closed = true;
+  path.smooth();
+  */
+}
 
 socket.on('other canvas change', function(data) {
-  console.log('other drawers positions');
-  console.log(data);
-  // redraw other players position in current canvas
+  if (data.type === 'mousedown') {
+    onMouseDown();
+  } else if (data.type === 'mousedrag') {
+    onMouseDrag(data);
+  } else if (data.type === 'mouseup') {
+    onMouseUp(data);
+  }
 });
+
