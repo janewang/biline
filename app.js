a606a9132eb0a7c77d3eb4db814e0c7f679dde44
Jane Wang <janelwang2008@gmail.com>
2012-10-20 00:01:42 -0400

    Add socket.io

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 #!/usr/local/bin/node
 
 var express   = require('express');
+var socketio  = require('socket.io');
 var app       = express.createServer();
 
 app.set('view engine', 'ejs');
@@ -14,3 +15,6 @@ app.get('/', function(request, response, next) {
 app.listen(3000, function() {
     console.log('Now listening on port 3000');    
 });
+
+var sio = socketio.listen(app, {log: false});
+console.log(sio);
