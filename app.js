1e939e4992b189d81817fe9d665cb943fba3c39c
Jane Wang <janelwang2008@gmail.com>
2012-10-19 23:55:10 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 #!/usr/local/bin/node
 
-var express = require('express');
-var app = express.createServer();
-console.log(__dirname + '/public');
+var express   = require('express');
+var app       = express.createServer();
+
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response, next) {
-  response.render('index.ejs', {layout: false});
+  response.render('index', {layout: false});
   response.end();
 });
 
-app.listen(3000);
-console.log('Now listening on 3000');
+app.listen(3000, function() {
+    console.log('Now listening on port 3000');    
+});
